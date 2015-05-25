f0d930246c64289887238aab32ef8a97b047528c
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-25 09:56:49 -0600

    Tutorial 79 on appending to a file

--- file.php
+++ file.php
@@ -1,8 +1,24 @@
 <?php
 
-$handle = fopen('names.txt', 'w');
-fwrite($handle, 'Billy' . "\n");
-fwrite($handle, 'Sargent');
-fclose($handle);
+if(isset($_POST['name'])){
+   $name = $_POST['name'];
+   if(!empty($name)){
+      $handle = fopen('names.txt', 'a');
+      fwrite($handle, $name . "\n");
+      fclose($handle);
+   }else{
+      echo 'Please type a name.';
+   }
+}
+
+
 
 ?>
+
+<form action="file.php" method="POST">
+   Name:<br>
+   <input type="text" name="name"><br><br>
+   <input type="submit" value="Submit">
+
+
+</form>
